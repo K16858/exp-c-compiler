fc783d7db9ced0a0df52518cdfefbce61a2a4410
k16858 <ktohda0127@gmail.com>
2025-12-08 22:50:14 +0900

    ヘッダー生成を追加

--- compiler/code_generation.c
+++ compiler/code_generation.c
@@ -16,9 +16,36 @@ void print_node(Node *n) {
     }
 }
 
+void gen_header() {
+    char* code = 
+"INITIAL_GP = 0x10008000 # initial value of global pointer\n"
+"INITIAL_SP = 0x7ffffffc # initial value of stack pointer\n"
+"# system call service number\n"
+"stop_service = 99\n"
+"\n"
+"       .text # テキストセグメントの開始\n"
+"init:\n"
+"    # initialize $gp (global pointer) and $sp (stack pointer)\n"
+"    la $gp, INITIAL_GP # $gp ←0x10008000 (INITIAL_GP)\n"
+"    la $sp, INITIAL_SP # $sp ←0x7ffffffc (INITIAL_SP)\n"
+"    jal main # jump to ‘main’\n"
+"    nop # (delay slot)\n"
+"    li $v0, stop_service # $v0 ←99 (stop_service)\n"
+"    syscall # stop\n"
+"    nop\n"
+"    # not reach here\n"
+"stop: # if syscall return\n"
+"    j stop # infinite loop...\n"
+"    nop # (delay slot)\n"
+"\n"
+".text 0x00001000 # 以降のコードを 0から配置 x00001000\n";
+    printf("%s", code);
+}
+
 void gen_code(Node *n) {
     printf("gen_code\n");
     print_node(n);
+    gen_header();
     if (n->child != NULL) {
         gen_code(n->child);
     }
