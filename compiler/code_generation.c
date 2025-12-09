eed1d8249c2ef7d52428179a9621e0ac8d425bbe
KosukeToda <pcgb6fve@s.okayama-u.ac.jp>
2025-12-09 11:03:19 +0900

    代入文を仮追加

--- compiler/code_generation.c
+++ compiler/code_generation.c
@@ -18,38 +18,65 @@ void print_node(Node *n) {
 
 void gen_header() {
     char* code = 
-"INITIAL_GP = 0x10008000 # initial value of global pointer\n"
-"INITIAL_SP = 0x7ffffffc # initial value of stack pointer\n"
-"# system call service number\n"
-"stop_service = 99\n"
-"\n"
-"       .text # テキストセグメントの開始\n"
-"init:\n"
-"    # initialize $gp (global pointer) and $sp (stack pointer)\n"
-"    la $gp, INITIAL_GP # $gp ←0x10008000 (INITIAL_GP)\n"
-"    la $sp, INITIAL_SP # $sp ←0x7ffffffc (INITIAL_SP)\n"
-"    jal main # jump to ‘main’\n"
-"    nop # (delay slot)\n"
-"    li $v0, stop_service # $v0 ←99 (stop_service)\n"
-"    syscall # stop\n"
-"    nop\n"
-"    # not reach here\n"
-"stop: # if syscall return\n"
-"    j stop # infinite loop...\n"
-"    nop # (delay slot)\n"
-"\n"
-".text 0x00001000 # 以降のコードを 0から配置 x00001000\n";
-    printf("%s", code);
+        "INITIAL_GP = 0x10008000 # initial value of global pointer\n"
+        "INITIAL_SP = 0x7ffffffc # initial value of stack pointer\n"
+        "# system call service number\n"
+        "stop_service = 99\n"
+        "\n"
+        "       .text # テキストセグメントの開始\n"
+        "init:\n"
+        "    # initialize $gp (global pointer) and $sp (stack pointer)\n"
+        "    la $gp, INITIAL_GP # $gp ←0x10008000 (INITIAL_GP)\n"
+        "    la $sp, INITIAL_SP # $sp ←0x7ffffffc (INITIAL_SP)\n"
+        "    jal main # jump to ‘main’\n"
+        "    nop # (delay slot)\n"
+        "    li $v0, stop_service # $v0 ←99 (stop_service)\n"
+        "    syscall # stop\n"
+        "    nop\n"
+        "    # not reach here\n"
+        "stop: # if syscall return\n"
+        "    j stop # infinite loop...\n"
+        "    nop # (delay slot)\n"
+        "\n"
+        ".text 0x00001000 # 以降のコードを 0から配置 x00001000\n"
+        "main:";
+    printf("%s\n", code);
+}
+
+void gen_assignment(n) {
+    char* code = "ori $t1, $zero, 0";
+
+    printf("%s\n", code);
 }
 
 void gen_code(Node *n) {
-    printf("gen_code\n");
-    print_node(n);
-    // gen_header();
-    if (n->child != NULL) {
-        gen_code(n->child);
+    // if (n->child != NULL) {
+    //     gen_code(n->child);
+    // }
+    // if (n->brother != NULL) {
+    //     gen_code(n->brother);
+    // }
+
+    if (n == NULL) {
+        return;
     }
-    if (n->brother != NULL) {
-        gen_code(n->brother);
+    printf("gen_code\n");
+    switch (n->type) {
+        case PROGRAM_AST:
+            print_node(n);
+            gen_header();
+            gen_code(n->child);
+            gen_code(n->brother);
+            break;
+        case ASSIGNMENT_STATEMENT_AST:
+            print_node(n);
+            gen_assignment(n);
+            gen_code(n->child);
+            gen_code(n->brother);
+            break;
+        default:
+            gen_code(n->child);
+            gen_code(n->brother);
+            break;
     }
 }
