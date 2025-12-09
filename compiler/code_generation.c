2c677988e02c882d1967fcc587680e945df08181
KosukeToda <pcgb6fve@s.okayama-u.ac.jp>
2025-12-09 16:19:29 +0900

    式の生成を仮追加

--- compiler/code_generation.c
+++ compiler/code_generation.c
@@ -10,6 +10,7 @@ void gen_fotter();
 void gen_decl_var(Node *n);
 void gen_assign(Node *n);
 void gen_loop(Node *n);
+void gen_expression(Node *n);
 void gen_code(Node *n);
 
 typedef struct {
@@ -67,8 +68,8 @@ void gen_header(Node *n) {
 
 void gen_fotter() {
     char* code = 
-        "   jr $ra\n"
-        "   nop\n"
+        "    jr $ra\n"
+        "    nop\n"
         "    \n"
         "    #data segment\n"
         "    .data 0x10004000\n"
@@ -100,6 +101,7 @@ int lookup_symbol_table(char *target_var) {
 
 void gen_decl_var(Node *n) {
     register_var(n);
+    gen_code(n->child);
     gen_code(n->brother);
 }
 
@@ -109,8 +111,8 @@ void gen_assignment(Node *n) {
         printf("No variable\n");
         return;
     }
-    printf("     ori $v0, $zero, 0\n");
-    printf("     sw $v0, %d($t0)\n", offset);
+    printf("    ori $v0, $zero, 0\n");
+    printf("    sw $v0, %d($t0)\n", offset);
 
     gen_code(n->child);
     gen_code(n->brother);
@@ -127,6 +129,60 @@ void gen_loop(Node *n) {
     printf("    j LOOP\n");
     printf("    nop\n");
     printf("EXIT:\n");
+}
+
+void gen_push() {
+    printf("    addi $sp, $sp, -4\n");
+    printf("    sw $v0, 0($sp)\n");
+}
+
+void gen_pop() {
+    printf("    lw $v0, 0($sp)\n");
+    printf("    addi $sp, $sp, 4\n");
+}
+
+void gen_var(Node *n) {
+    printf("    lw $t0, %d($t0)");
+}
+
+void gen_number(Node *n) {
+    int num = n->ivalue;
+    printf("    ori $t0, $zero, %d", num);
+}
+
+void gen_expression(Node *n) {
+    // switch (n->type) {
+    //     case NUMBER_AST:
+    //         printf("NUMBER\n");
+    //         gen_code(n->child);
+    //         break;
+    //     case IDENT_AST:
+    //         printf("IDENT\n");
+    //         gen_code(n->child);
+    //     default:
+    //         break;
+    // }
+    gen_code(n->child);
+    gen_push();
+    gen_code(n->brother);
+    printf("    ori $v1, $zero, $v0\n");
+    gen_pop();
+    switch (n->type) {
+        case ADD_OP_AST:
+            printf("    add $v0, $v0, $t1\n");
+            break;
+        case SUB_OP_AST:
+            printf("    sub $v0, $v0, $t1\n");
+            break;
+        case MUL_OP_AST:
+            printf("    mult $v0, $v0, $t1\n");
+            break;
+        case DIV_OP_AST:
+            printf("    div $v0, $v0, $t1\n");
+            break;
+        default:
+            break;
+    }
 }
 
 void gen_code(Node *n) {
@@ -161,8 +217,17 @@ void gen_code(Node *n) {
         case CONDITION_AST:
             printf("    CONDITION\n");
             break;
+        case IDENT_AST:
+        case NUMBER_AST:
+            gen_number(n);
+            break;
+        case ADD_OP_AST:
+        case SUB_OP_AST:
+        case MUL_OP_AST:
+        case DIV_OP_AST:
+            gen_expression(n);
+            break;
         default:
-            print_node(n);
             gen_code(n->child);
             gen_code(n->brother);
             break;
